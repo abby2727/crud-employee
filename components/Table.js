ab3b13bc7e8802bd88de8d2fc42bd50129976ecc
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-03-03 17:14:40 +0800

    Update

--- components/Table.js
+++ components/Table.js
@@ -36,7 +36,7 @@ const Table = () => {
                     <td className="px-16 py-2">
                         <span>23</span>
                     </td>
-                    <td className="px-16 py-2 flex justify-around gap-2">
+                    <td className="px-20 py-2 flex justify-around gap-2">
                         <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
                         <button className="cursor"><BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt></button>
                     </td>
