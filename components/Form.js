117a87a2bed96c97fec9360aba2c1fe595b2deb7
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-03-03 17:25:15 +0800

    useReducer Hook - geting form data

--- components/Form.js
+++ components/Form.js
@@ -1,17 +1,34 @@
+import { useReducer } from "react"
+import { BiPlus } from 'react-icons/bi'
+
+const formReducer = (state, event) => {
+    return {
+        ...state,
+        [event.target.name]: event.target.value
+    }
+}
+
 const Form = () => {
+    const [formData, setFormData] = useReducer(formReducer, {})
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData)
+    }
+
     return (
-        <form className="grid lg:grid-cols-2 w-4/6 gap-4">
+        <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
             <div className="input-type">
-                <input type="text" name="firstname" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="First Name" />
+                <input type="text" name="firstname" onChange={setFormData} className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="First Name" />
             </div>
             <div className="input-type">
-                <input type="text" name="lastname" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Last Name" />
+                <input type="text" name="lastname" onChange={setFormData} className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Last Name" />
             </div>
             <div className="input-type">
-                <input type="day" name="birthday" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Birthday" />
+                <input type="date" name="birthday" onChange={setFormData} className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Birthday" />
             </div>
             <div className="input-type">
-                <input type="text" name="age" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Age" />
+                <input type="text" name="age" onChange={setFormData} className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Age" />
             </div>
             <button className="flex justify-center text-md w-2/6 bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">Add</button>
         </form>
--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         <div className="container mx-auto flex justify-between py-5">
           <div className="left flex gap-3">
             <button className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800'>
-              Add Employee <span className='px-1'><BiUserPlus size={23} /></span>
+              Add <span className='px-1'><BiUserPlus size={23} /></span>
             </button>
           </div>
         </div>
